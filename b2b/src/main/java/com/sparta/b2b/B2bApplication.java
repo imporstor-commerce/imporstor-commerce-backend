ff5f1e4d7f5a018742f19014e06915e2eb6d1baa
nurigaon96 <nurigaon96@gmail.com>
2024-12-05 23:30:34 +0900

    [#11] feat: B2B 화면에서 주문 리스트 조회

--- b2b/src/main/java/com/sparta/b2b/B2bApplication.java
+++ b2b/src/main/java/com/sparta/b2b/B2bApplication.java
@@ -3,8 +3,10 @@ package com.sparta.b2b;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = "com.sparta.impostor.commerce.backend", exclude = { SecurityAutoConfiguration.class })
+@EnableJpaAuditing
+@SpringBootApplication(scanBasePackages = "com.sparta", exclude = { SecurityAutoConfiguration.class })
 public class B2bApplication {
 
 	public static void main(String[] args) {
